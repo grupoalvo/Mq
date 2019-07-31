c39b89495544d413682b2331811c0cb3b3b0a755
GrupoAlvo <52456433+grupoalvo@users.noreply.github.com>
2019-07-31 13:47:34 -0300

    Add files via upload

---
+++ scripttop02b01.js
@@ -0,0 +1,426 @@
+function _defineProperty(obj, key, value) {if (key in obj) {Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true });} else {obj[key] = value;}return obj;}const answerObj = [{
+    question: '(FCC/2019 - Adaptada) Com relação às Normas Gerais de Circulação e Conduta, nas vias urbanas e nas rurais de pista dupla, a circulação de bicicletas deverá ocorrer nos bordos da pista de rolamento em sentido contrário de circulação regulamentado para a via, com preferência sobre os veículos automotores.',
+    answers: [
+    'Certo',
+    'Errado'],
+    
+    correct: [2],
+    messages: {
+      pass: 'Parabéns! Você acertou.',
+      fail: 'Você errou. Segundo Art. 58 do CTB: Nas vias urbanas e nas rurais de pista dupla, a circulação de bicicletas deverá ocorrer, quando não houver ciclovia, ciclofaixa, ou acostamento, ou quando não for possível a utilização destes, nos bordos da pista de rolamento, no mesmo sentido de circulação regulamentado para a via, com preferência sobre os veículos automotores.' } },
+  
+  {
+    question: '(FCC/2019 - Adaptada) Com relação às Normas Gerais de Circulação e Conduta, desde que autorizado e devidamente sinalizado pelo órgão ou entidade com circunscrição sobre a via, será permitida a circulação de bicicletas nos passeios.',
+    answers: [
+    'Certo',
+    'Errado'],
+  
+    correct: [1],
+    messages: {
+      pass: 'Parabéns! Você acertou.',
+      fail: 'Você errou. Segundo o Art. 59, do CTB: Desde que autorizado e devidamente sinalizado pelo órgão ou entidade com circunscrição sobre a via, será permitida a circulação de bicicletas nos passeios.' } },
+  
+  {
+    question: '(CESPE/2017) Na condução de um veículo, ao se aproximar de um cruzamento não sinalizado, o motorista deverá observar os veículos que trafegam na pista transversal e dar preferência de passagem ao que vier pela via da esquerda.',
+    answers: [
+    'Certo',
+    'Errado'],
+  
+    correct: [2],
+    messages: {
+      pass: 'Parabéns! Você acertou.',
+      fail: 'Você errou. Segundo o Art. 29 do CTB: III - quando veículos, transitando por fluxos que se cruzem, se aproximarem de local não sinalizado, terá preferência de passagem: (...) c) nos demais casos, o que vier pela direita do condutor.' } },
+  
+  {
+    question: '(CESPE/2017) Na condução de um veículo, ao se aproximar de um cruzamento não sinalizado, o motorista deverá acelerar para chegar antes do cruzamento, caso outro motorista esteja sinalizando que irá ultrapassá-lo.',
+    answers: [
+    'Certo',
+    'Errado'],
+  
+    correct: [2],
+    messages: {
+      pass: 'Parabéns! Você acertou.',
+      fail: 'Você errou. Segundo o Art. 44 do CTB: Ao aproximar-se de qualquer tipo de cruzamento, o condutor do veículo deve demonstrar prudência especial, transitando em velocidade moderada, de forma que possa deter seu veículo com segurança para dar passagem a pedestre e a veículos que tenham o direito de preferência.' } },
+  
+  {
+    question: '(CESPE/2017- Adaptada) Nas vias em que seja proibido estacionar, a parada de veículos poderá ser feita, pelo tempo necessário à carga e à descarga.',
+    answers: [
+    'Certo',
+    'Errado'],
+  
+    correct: [2],
+    messages: {
+      pass: 'Parabéns! Você acertou.',
+      fail: 'Você errou. Segundo o Art. 47 do CTB: Parágrafo único. A operação de carga ou descarga será regulamentada pelo órgão ou entidade com circunscrição sobre a via e é considerada estacionamento.' } },
+  
+  {
+    question: '(FADESP/2019 - Adaptada) A circulação do trânsito de veículos far-se-á pelo lado direito da via, sem exceções.',
+    answers: [
+    'Certo',
+    'Errado'],
+  
+    correct: [2],
+    messages: {
+      pass: 'Parabéns! Você acertou.',
+      fail: 'Você errou. Há exceções: Ultrapassagem .' } },
+  
+  {
+    question: '(FADESP/2019 - Adaptada) Os veículos de transporte coletivo regular de passageiros, quando circularem em faixas a eles destinadas, e os ciclos motorizados deverão utilizar-se de farol de luz baixa durante o dia e a noite.',
+    answers: [
+    'Certo',
+    'Errado'],
+  
+    correct: [1],
+    messages: {
+      pass: 'Parabéns! Você acertou.',
+      fail: 'Você errou. Segundo o Art. 40 do CTB: Parágrafo único. Os veículos de transporte coletivo regular de passageiros, quando circularem em faixas próprias a eles destinadas, e os ciclos motorizados deverão utilizar-se de farol de luz baixa durante o dia e a noite.' } },
+  
+  {
+    question: '(FUNDATEC/2018 - Adaptada) Para cruzar a pista de rolamento, o pedestre tomará precauções de segurança, levando em conta, principalmente, a visibilidade, a distância e a velocidade dos veículos.',
+    answers: [
+    'Certo',
+    'Errado'],
+  
+    correct: [1],
+    messages: {
+      pass: 'Parabéns! Você acertou.',
+      fail: 'Você errou. Segundo o Art. 69 do CTB:  Para cruzar a pista de rolamento o pedestre tomará precauções de segurança, levando em conta, principalmente, a visibilidade, a distância e a velocidade dos veículos(...).' } },
+  
+  {
+    question: '(CESPE/2015) A classificação das vias urbanas é feita de acordo com a sua utilização e característica, constitui critério de fixação de limite de velocidade de cada tipo de via e estabelece parâmetros e condições de preferência de passagem em cruzamentos desprovidos de sinalização.',
+    answers: [
+    'Certo',
+    'Errado'],
+  
+    correct: [2],
+    messages: {
+      pass: 'Parabéns! Você acertou.',
+      fail: 'Você errou. Segundo o Art. 29 do CTB: O trânsito de veículos nas vias terrestres abertas à circulação obedecerá às seguintes normas: (...) c) nos demais casos, o que vier pela direita do condutor. Não depende só do tipo de via.' } },
+  
+  {
+    
+    question: '(CESPE/2015) A buzina, um agente causador de poluição sonora, poderá ser usada pelo condutor do veículo desde que em toque breve, para fazer advertências necessárias e evitar acidentes; contudo, usar a buzina entre 22 h e 6 h configura infração de trânsito.',
+    answers: [
+    'Certo',
+    'Errado'],
+  
+    correct: [1],
+    messages: {
+      pass: 'Parabéns! Você acertou.',
+      fail: 'Você errou. Segundo o Art. 227 do CTB: Consta infração de trânsito usar buzina: (...) III - entre as vinte e duas e as seis horas;.' } }];
+  
+  
+  var ReactCSS = React.addons.CSSTransitionGroup;
+  
+  const Controls = ({
+    currentQuestion,
+    userAnswers,
+    parentMoveBack,
+    numQuestions,
+    parentMoveForward,
+    parentFinishTest,
+    finishedAnswering }) =>
+  {
+  
+    return (
+      React.createElement("div", null,
+      currentQuestion > 0 ? React.createElement("button", { className: "btn", onClick: parentMoveBack, id: "prev" }, "Questão Anterior") : '',
+      currentQuestion < numQuestions - 1 ? React.createElement("button", { className: "btn", onClick: parentMoveForward, id: "next" }, "Próxima Questão") : '',
+  
+      !finishedAnswering && currentQuestion == numQuestions - 1 ? React.createElement("p", null, "Responda todas as questões para continuar") : ''));
+  
+  
+  
+  
+  };
+  Controls.propTypes = {
+    currentQuestion: React.PropTypes.number.isRequired,
+    numQuestions: React.PropTypes.number.isRequired,
+    userAnswers: React.PropTypes.array.isRequired,
+    parentMoveBack: React.PropTypes.func.isRequired,
+    parentMoveForward: React.PropTypes.func.isRequired,
+    parentFinishTest: React.PropTypes.func.isRequired,
+    testForUndefined: React.PropTypes.func.isRequired };
+  
+  
+  class ScoreArea extends React.Component {
+    constructor(props) {
+      super(props);_defineProperty(this, "componentWillMount",
+  
+  
+  
+  
+  
+      () => {
+        const userAnswers = this.props.userAnswers;
+        const userAnswerLen = userAnswers.length;
+        const theQuestions = this.props.allQuestions;
+        let score = this.state.score;
+        let passFail = [];
+        for (let i = 0; i < userAnswerLen; i++) {
+          let correctAnswers = theQuestions[i].correct;
+          const corrAnsLen = correctAnswers.length;
+          passFail[i] = 'fail';
+          for (let x = 0; x < corrAnsLen; x++) {
+  
+            if (theQuestions[i].correct[x] == userAnswers[i]) {
+              score++;
+              passFail[i] = 'pass';
+            }
+          }
+        }
+  
+        this.setState({
+          score: score,
+          passFail: passFail });
+  
+      });_defineProperty(this, "render",
+  
+      () => {
+        let theUserAnswers = this.props.userAnswers;
+        return (
+          React.createElement("div", null,
+          React.createElement("p", { id: "your-score-lab", className: "score-p" }, "Sua Pontuação foi: ", this.state.score, " / ", this.props.allQuestions.length),
+          React.createElement("table", null,
+          React.createElement("tbody", null,
+          this.props.allQuestions.map(function (question, i) {
+            return (
+              React.createElement("tr", null,
+              React.createElement("td", { className: "result-question-num" },
+              React.createElement("p", null, i + 1)),
+  
+              React.createElement("td", null,
+              React.createElement("div", { className: "feedback", key: question.answers[this.props.userAnswers[i]] + 'top-div' },
+              React.createElement("h4", { key: question.answers[theUserAnswers[i]] + 'first-p' }, question.question),
+              React.createElement("p", { key: question.answers[theUserAnswers[i]] + 'last-p', className: this.state.passFail[i] }, question.answers[theUserAnswers[i]]),
+              React.createElement("p", { key: 'error-' + i }, question.messages[this.state.passFail[i]])))));
+  
+  
+  
+  
+          }, this)))));
+  
+  
+  
+  
+      });this.state = { score: 0, passFail: [] };}}
+  
+  
+  ScoreArea.propTypes = {
+  
+    userAnswers: React.PropTypes.array.isRequired,
+    allQuestion: React.PropTypes.object.isRequired };
+  
+  
+  
+  const Question = ({
+    parentAnswerClick,
+    questionNum,
+    questionBlockNumber,
+    userAnswers,
+    option }) =>
+  {
+  
+    return (
+      React.createElement("div", { className: userAnswers[questionBlockNumber] == questionNum ? "user-choice selectedans" : "user-choice",
+        onClick: parentAnswerClick,
+        "data-question-num": questionNum,
+        "data-block-num": questionBlockNumber },
+  
+      React.createElement("div", { className: userAnswers[questionBlockNumber] == questionNum ? "checkbox selected" : "checkbox" }),
+  
+  
+      React.createElement("p", { className: userAnswers[questionBlockNumber] == questionNum ? "quiz-answer selected" : "quiz-answer" },
+      option)));
+  
+  
+  
+  
+  
+  };
+  Question.propTypes = {
+    parentAnswerClick: React.PropTypes.func.isRequired,
+    questionNum: React.PropTypes.number.isRequired,
+    questionBlockNumber: React.PropTypes.number.isRequired,
+    option: React.PropTypes.number.isRequired,
+    userAnswers: React.PropTypes.array.isRequired };
+  
+  
+  const QuestionBlock = ({
+    setUserAnswers,
+    question,
+    questionBlockNumber,
+    userAnswers,
+    testForUndefined }) =>
+  {
+  
+    const onAnswerClick = e => {
+      var blockNum = e.currentTarget.dataset.blockNum;
+      var selection = e.currentTarget.dataset.questionNum;
+      setUserAnswers(blockNum, selection);
+      testForUndefined();
+    };
+  
+    let thePotentialAnswers = question.answers.map(function (answer, i) {
+      return (
+        React.createElement(Question, { questionNum: i,
+          questionBlockNumber: questionBlockNumber,
+          parentAnswerClick: onAnswerClick,
+          userAnswers: userAnswers,
+          key: answer,
+          option: answer }));
+  
+    }, this);
+  
+    const transitionOptions = {
+      transitionName: "fade",
+      transitionEnterTimeout: 300,
+      transitionLeaveTimeout: 100 };
+  
+  
+    return (
+      React.createElement("div", { className: "assessment-question" },
+      React.createElement("h4", null, question.question),
+      React.createElement("div", { className: "answer-holder" },
+      React.createElement(ReactCSS, transitionOptions,
+      thePotentialAnswers))));
+  
+  
+  
+  
+  
+  };
+  
+  QuestionBlock.propTypes = {
+    setUserAnswers: React.PropTypes.func.isRequired,
+    question: React.PropTypes.object.isRequired,
+    questionBlockNumber: React.PropTypes.number.isRequired,
+    userAnswers: React.PropTypes.array.isRequired,
+    testForUndefind: React.PropTypes.func.isRequired };
+  
+  
+  const QuestionArea = ({
+    userAnswers,
+    parentSetUserAnswers,
+    currentQuestion,
+    allQuestions,
+    testForUndefined }) =>
+  {
+    return (
+      React.createElement("div", { id: "question-holder" },
+      React.createElement(QuestionBlock, { userAnswers: userAnswers,
+        setUserAnswers: parentSetUserAnswers,
+        testForUndefined: testForUndefined,
+        questionBlockNumber: currentQuestion,
+        question: allQuestions[currentQuestion] })));
+  
+  
+  
+  };
+  QuestionArea.propTypes = {
+    userAnswers: React.PropTypes.array.isRequired,
+    currentQuestion: React.PropTypes.number.isRequired,
+    allQuestions: React.PropTypes.object.isRequired,
+    parentSetUserAnswers: React.PropTypes.func.isRequired,
+    testForUndefined: React.PropTypes.func.isRequired };
+  
+  
+  class Test extends React.Component {
+    constructor(props) {
+      super(props);_defineProperty(this, "moveForward",
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+      () => {
+        let curQuest = this.state.currentQuestion;
+        if (curQuest <= this.state.numQuestions - 1) {
+          this.setState({
+            currentQuestion: this.state.currentQuestion + 1 });
+  
+        }
+      });_defineProperty(this, "moveBack",
+  
+      () => {
+        let curQuest = this.state.currentQuestion;
+        if (curQuest > 0) {
+          this.setState({
+            currentQuestion: this.state.currentQuestion - 1 });
+  
+        }
+      });_defineProperty(this, "finishTest",
+  
+      () => {
+        this.setState({
+          quizComplete: 1 });
+  
+      });_defineProperty(this, "setUserAnswers",
+  
+      (a, b) => {
+        let currArray = this.state.userAnswers;
+        currArray[a] = b;
+  
+        this.setState({
+          userAnswers: currArray });
+  
+  
+      });_defineProperty(this, "testForUndefined",
+      () => {
+  
+        let filled = true;
+        for (let x = 0; x < answerObj.length; x++) {
+          if (typeof this.state.userAnswers[x] == 'undefined') {
+            filled = false;
+  
+          }
+        }
+  
+        this.setState({
+          finishedAnswering: filled });
+  
+  
+        return filled;
+      });_defineProperty(this, "render",
+  
+      () => {
+        return (
+          React.createElement("div", { id: "quizHolder" },
+          !this.state.quizComplete ?
+  
+          React.createElement("div", null,
+  
+          React.createElement(QuestionArea, { parentSetUserAnswers: this.setUserAnswers,
+            userAnswers: this.state.userAnswers,
+            currentQuestion: this.state.currentQuestion,
+            allQuestions: answerObj,
+            testForUndefined: this.testForUndefined }),
+  
+  
+          React.createElement(Controls, { parentMoveForward: this.moveForward,
+            userAnswers: this.state.userAnswers,
+            parentMoveBack: this.moveBack,
+            parentFinishTest: this.finishTest,
+            finishedAnswering: this.state.finishedAnswering,
+            currentQuestion: this.state.currentQuestion,
+            numQuestions: this.state.numQuestions }),
+  
+          this.state.finishedAnswering && React.createElement("button", { className: "btn", id: "finish", onClick: this.finishTest }, "Finalizar")) :
+  
+          React.createElement(ScoreArea, { userAnswers: this.state.userAnswers,
+            allQuestions: answerObj })));
+  
+  
+  
+  
+      });this.state = { currentQuestion: 0, userAnswers: [], finishedAnswering: false, quizComplete: 0, numQuestions: answerObj.length };}}
+  
+  
+  ReactDOM.render(
+  React.createElement(Test, null),
+  document.getElementById('test-div'));
